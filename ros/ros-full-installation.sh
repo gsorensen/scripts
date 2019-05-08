3e1d27917077ad37932d67f0eb5bc33e07228685
Glen H. M. Sørensen <gsorensen1201@gmail.com>
2019-05-08 16:30:26 +0200

    altered script to allow mint support

--- ros/ros-full-installation.sh
+++ ros/ros-full-installation.sh
@@ -2,9 +2,10 @@
 # http://wiki.ros.org/melodic/Installation/Ubuntus
 
 # Setting up sources.list
-# Note, this can be done on Mint, but ubuntu lsb_release need to change to 
-# mint distro i.e. sylvia or tara
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# Note, this can be done on Mint, but ubuntu lsb_release needs to be the ubuntu base of that mint repo. Set this below
+UBUNTU_BASE = "bionic"
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(UBUNTU_BASE) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # Set up keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
